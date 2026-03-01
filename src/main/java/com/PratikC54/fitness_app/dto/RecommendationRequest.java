d00ef3bcab45061a9bc202867ed562b64dbb5bd6
PratikC54 <pratiksenapati54@gmail.com>
2026-03-01 12:11:15 +0530

    Creating request DTO for recommendation related services

---
+++ src/main/java/com/PratikC54/fitness_app/dto/RecommendationRequest.java
@@ -0,0 +1,26 @@
+package com.PratikC54.fitness_app.dto;
+
+import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RecommendationRequest {
+
+    private String userId;
+    private String activityId;
+    private String type;
+    private String recommendation;
+
+    private List<String> improvements;
+
+    private List<String> suggestions;
+
+    private List<String> safety;
+
+}
