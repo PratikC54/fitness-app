6bd8750e93af8f886d3ba0a58b0b44f59dfa1d8c
PratikC54 <pratiksenapati54@gmail.com>
2026-03-01 12:11:41 +0530

    updating recommendation model

--- src/main/java/com/PratikC54/fitness_app/model/Recommendation.java
+++ src/main/java/com/PratikC54/fitness_app/model/Recommendation.java
@@ -3,9 +3,12 @@ package com.PratikC54.fitness_app.model;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -16,6 +19,7 @@ import static org.hibernate.type.SqlTypes.JSON;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class Recommendation {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
@@ -38,7 +42,9 @@ public class Recommendation {
     @Column(columnDefinition = "json")
     private List<String> safety;
 
+    @CreationTimestamp
     private LocalDateTime createdAt;
+    @UpdateTimestamp
     private LocalDateTime updatedAt;
 
     @ManyToOne(fetch = FetchType.LAZY)
@@ -50,4 +56,6 @@ public class Recommendation {
     @JoinColumn(name = "activity_id" , nullable = false , foreignKey = @ForeignKey(name = "fk_recommendation_activity"))
     @JsonIgnore
     private Activity activity;
+
+
 }
